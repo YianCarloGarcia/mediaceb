99baeb2a02ee2d0aa5e7bb87af6114d1b537a437
YianCarloGarcia <juanscrooge@gmail.com>
2026-03-31 23:24:05 -0500

    sin S

--- sistema/settings.py
+++ sistema/settings.py
@@ -153,6 +153,6 @@ if DATABASE_URL:
     DATABASES['default'] = dj_database_url.parse(DATABASE_URL)
 
 # Archivos estáticos con WhiteNoise
-MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseSMiddleware')
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
